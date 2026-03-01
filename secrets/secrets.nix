5995b3a6a70e6deca35bdbb4c0fff3c836380ab1
Robert Jarske Eriksson <jarske.eriksson@gmail.com>
2026-03-01 22:42:53 +0100

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   yubikey2 = "age1yubikey1q233vgvezsfxkyat8cvwwcmzcnww3rhj4d3qss5vfv5yyh9q06kw5rch6ux";
   masters = [ yubikey1 yubikey2 ];
 
-  bastion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJZSpzYiejmj9maOjbF7XwZIKjbcxARea8juP4P+dvj";
+  bastion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCCnBAD6+Ec/uxrXbK4OEPCKLCQn8TmNwdK5PiYwDdl root@nixos";
   # forge = "ssh-ed25519 AAAA...";  # add after first install
 in
 {
