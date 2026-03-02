55066431b1cc80d069ea95a999c9cda51138b276
Robert Jarske Eriksson <jarske.eriksson@gmail.com>
2026-03-02 15:30:14 +0100

    chore: add bastion age key, rekey secrets, add bastion hardware config

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   yubikey2 = "age1yubikey1q233vgvezsfxkyat8cvwwcmzcnww3rhj4d3qss5vfv5yyh9q06kw5rch6ux";
   masters = [yubikey1 yubikey2];
 
-  bastion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFF3Vlg/2qfU1jd3lSLu3CNKQpTnjtu3QUtVejSYvIw root@nixos";
+  bastion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE6mrKLP7qxqaQfIKZbbVx6LyNTLRPYG+MEP+Zcm6Ln root@nixos";
   # forge = "ssh-ed25519 AAAA...";  # add after first install
 in {
   "wifi-blackbox.age".publicKeys = masters ++ [bastion]; # ++ [ forge ] after first install
